dbe982a6f40b7aac5d9983da22b736b43971a02f
HUGO-DSH-tec <hugo.dossantoshoquet@gmail.com>
2024-10-10 14:33:32 +0200

    Add files via upload

---
+++ MaFenetre.h
@@ -0,0 +1,23 @@
+
+#include <QtWidgets> /*tousles widgets de Qt5 */
+#include <QtGui> /*tous leswidgets de Qt4 */
+#define CELCIUS_FARENHEIT 0
+#define FARENHEIT_CELCIUS 1
+
+     class MaFenetre : public QDialog
+    {
+        Q_OBJECT
+        // Membre(s) public(s)
+    public:
+        MaFenetre( QWidget *parent = 0 );
+        // Membre(s) privé(s)
+    private:
+        // Les widgets
+        QLineEdit *valeur;
+        QLabel *resultat;
+        QLabel *unite;
+        QComboBox *choixConversion;
+        QPushButton *bConvertir;
+        QPushButton *bQuitter;
+        QDoubleValidator *doubleValidator;
+    };
